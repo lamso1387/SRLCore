f0663143d1647d953ad8c09a09675dff88cbb86c
lamso1387 <soheillamso@gmail.com>
2022-11-26 18:29:29 +0330

    baskol_directPayCons

--- Model/Constants.cs
+++ Model/Constants.cs
@@ -8,6 +8,11 @@ namespace SRLCore
 {
     public class Constants
     {
+        public class SqlConstarint
+        {
+            public static string PersianDateString(string column_name) =>
+            $"([{column_name}] like '1[34][0-9][0-9]/_____' AND([{column_name}] like '_____1[0-2]/[12][0-9]' OR[{column_name}] like '_____0[1-9]/[12][0-9]' OR[{column_name}] like '_____1[0-2]/3[01]' OR[{column_name}] like '_____0[1-9]/3[01]' OR[{column_name}] like '_____1[0-2]/0[1-9]' OR[{column_name}] like '_____0[1-9]/0[1-9]'))";
+        }
         public class MessageText
         {
             public const string RequiredFieldError = "فیلد اجباری تکمیل نشده است";
