e07d19249d553f5295ec2760370e44eef1a2162f
lamso1387 <soheillamso@gmail.com>
2023-03-29 09:43:37 +0430

    barname no

--- Model/Constants/SqlConstarint.cs
+++ Model/Constants/SqlConstarint.cs
@@ -8,6 +8,7 @@ namespace SRLCore.Model.Constants
     {
         public static string PersianDateString(string column_name) =>
         $"([{column_name}] like '1[34][0-9][0-9]/_____' AND([{column_name}] like '_____1[0-2]/[12][0-9]' OR[{column_name}] like '_____0[1-9]/[12][0-9]' OR[{column_name}] like '_____1[0-2]/3[01]' OR[{column_name}] like '_____0[1-9]/3[01]' OR[{column_name}] like '_____1[0-2]/0[1-9]' OR[{column_name}] like '_____0[1-9]/0[1-9]'))";
-        public static string OfficialBarnameNo(string column_name) => $"([{column_name}] like '14__/__-______'";
+        public static string OfficialBarnameNoConstraint="'14[0-9][0-9]/[0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]'";
+        public static string OfficialBarnameNo(string column_name) => $"([{column_name}] like {OfficialBarnameNoConstraint}";
     }
 }
