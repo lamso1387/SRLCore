07fd2bb1e21c31522f892652a24714484c80190c
lamso1387 <soheillamso@gmail.com>
2023-04-18 18:05:24 +0430

    convertor

---
+++ Tools/Convertor/EnumDescriptionNumberToDic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SRLCore.Tools
+{
+    public class Convertor
+    {
+        public static Dictionary<int, string> EnumDescriptionNumberToDic(Type enum_type)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            foreach (var val in Enum.GetValues(enum_type))
+            {
+                string enum_des_str = SRL.ClassManagement.GetEnumDescription(val);
+                dic.Add((int)val, enum_des_str);
+            }
+            return dic;
+
+        }
+    }
+}
